3338c468fdaba78e49787f9cb456483bf662f6b6
epsilon <epsilon@b9806fc4-da95-481b-a3ce-6258aed95de6>
2011-06-13 09:21:26 +0000

    将AreaCoordTriangleQuadrature类更名为TriangleSymmetricQua，并增加工具函数。在EyMath中加入triangleArea函数便利计算三角形面积。 发现SimpMeshfree在很久以前就运行不能了，Ft!
    git-svn-id: svn://epsilony.net/epsilonRepos/JavaUtils/trunk@155 b9806fc4-da95-481b-a3ce-6258aed95de6

--- src/net/epsilony/math/util/TriangleSymmetricQuadrature.java
+++ src/net/epsilony/math/util/TriangleSymmetricQuadrature.java
@@ -5,22 +5,27 @@
 package net.epsilony.math.util;
 
 import java.util.Arrays;
+import org.apache.commons.math.FunctionEvaluationException;
+import org.apache.commons.math.analysis.BivariateRealFunction;
 
 /**
- *
+ * the data is copy from: </ br>
+ * D.A. Dunavant. High degree efficient symmetrical Gaussian quadrature rules for thetriangle. International Journal of Numerical Methods in Engineering, 22:1129-1148
  * @author epsilon
  */
-public class AreaCoordTriangleQuadrature {
+public class TriangleSymmetricQuadrature {
 
     final static int[] numPts = new int[]{1, 3, 4, 6, 7, 12, 13};
-    final static double[][] weights = new double[][]{{1},
+    final static double[][] weights = new double[][]{
+        {1},
         {1 / 3d, 1 / 3d, 1 / 3d},
         {-27 / 48d, 25 / 48d, 25 / 48d, 25 / 48d},
         {0.109951743655322, 0.109951743655322, 0.109951743655322, 0.223381589678011, 0.223381589678011, 0.223381589678011},
         {0.225000000000000, 0.125939180544827, 0.125939180544827, 0.125939180544827, 0.132394152788506, 0.132394152788506, 0.132394152788506},
         {0.050844906370207, 0.050844906370207, 0.050844906370207, 0.116786275726379, 0.116786275726379, 0.116786275726379, 0.082851075618374, 0.082851075618374, 0.082851075618374, 0.082851075618374, 0.082851075618374, 0.082851075618374},
         {-0.149570044467670, 0.175615257433204, 0.175615257433204, 0.175615257433204, 0.053347235608839, 0.053347235608839, 0.053347235608839, 0.077113760890257, 0.077113760890257, 0.077113760890257, 0.077113760890257, 0.077113760890257, 0.077113760890257}};
-    final static double[][] areaCoods = new double[][]{{1 / 3d, 1 / 3d, 1 / 3d},
+    final static double[][] barycentricCoordinates = new double[][]{
+        {1 / 3d, 1 / 3d, 1 / 3d},
         {2 / 3d, 1 / 6d, 1 / 6d, 1 / 6d, 2 / 3d, 1 / 6d, 1 / 6d, 1 / 6d, 2 / 3d},
         {1 / 3d, 1 / 3d, 1 / 3d, 0.6, 0.2, 0.2, 0.2, 0.6, 0.2, 0.2, 0.2, 0.6},
         {0.816847572980459, 0.091576213509771, 0.091576213509771, 0.091576213509771, 0.816847572980459, 0.091576213509771, 0.091576213509771, 0.091576213509771, 0.816847572980459, 0.108103018168070, 0.445948490915965, 0.445948490915965, 0.445948490915965, 0.108103018168070, 0.445948490915965, 0.445948490915965, 0.445948490915965, 0.108103018168070},
@@ -34,12 +39,46 @@ public class AreaCoordTriangleQuadrature {
 //        }
 //    }
 
-    public static double[] getAreaCoordinates(int nStep) {
-        return Arrays.copyOf(areaCoods[nStep - 1], areaCoods[nStep - 1].length);
-
+    public static double[] getBarycentricCoordinates(int power, boolean copy) {
+        if (copy) {
+            return Arrays.copyOf(barycentricCoordinates[power - 1], barycentricCoordinates[power - 1].length);
+        } else {
+            return barycentricCoordinates[power - 1];
+        }
     }
 
-    public static double[] getWeights(int nStep) {
-        return Arrays.copyOf(weights[nStep - 1], weights[nStep - 1].length);
+    public static double[] getBarycentricCoordinates(int power) {
+        return getBarycentricCoordinates(power, false);
+    }
+
+    public static double[] getWeights(int power, boolean copy) {
+        if (copy) {
+            return Arrays.copyOf(weights[power - 1], weights[power - 1].length);
+        } else {
+            return weights[power - 1];
+        }
+    }
+    
+    public static double[] getWeights(int power){
+        return getWeights(power,false);
+    }
+
+    public static double quadrate(double x1, double y1, double x2, double y2, double x3, double y3, int power, BivariateRealFunction fun, double area) throws FunctionEvaluationException {
+        int numPt = numPts[power - 1];
+        double[] coords = barycentricCoordinates[power -1];
+        double[] ws= weights[power-1];
+        double result=0;
+        for (int i = 0; i < numPt; i++) {
+            double x = x1 *coords[i*3]+x2*coords[i*3+1]+x3*coords[i*3+2];
+            double y = y1 *coords[i*3]+y2*coords[i*3+1]+y3*coords[i*3+2];
+            result+=ws[i]*fun.value(x, y);
+        }
+        result*=area;
+        return result;
+    }
+    
+    public static double quadrate(double x1, double y1,double x2,double y2,double x3,double y3,int power,BivariateRealFunction fun) throws FunctionEvaluationException{
+        double area = Math.abs((x1 - x2) * (y3 - y2) - (x3 - x2) * (y1 - y2)) / 2;
+        return quadrate(x1, y1, x2, y2, x3, y3, power, fun, area);
     }
 }
