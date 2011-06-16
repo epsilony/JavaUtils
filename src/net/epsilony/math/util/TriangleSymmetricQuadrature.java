417e3bd54bfe4e4621c90c3af612a08ebfd87de3
epsilon <epsilon@b9806fc4-da95-481b-a3ce-6258aed95de6>
2011-06-16 13:55:08 +0000

    完善了TriangleSymmetricQuadrature，测试已经可以对小于等于5阶的二维多项式得到不小于十二位有效数字解。新建了SimpsonIntegratorOnTriangleDomain，该类草创，的是测试TriangleSymmetricQuadrature。
    git-svn-id: svn://epsilony.net/epsilonRepos/JavaUtils/trunk@169 b9806fc4-da95-481b-a3ce-6258aed95de6

--- src/net/epsilony/math/util/TriangleSymmetricQuadrature.java
+++ src/net/epsilony/math/util/TriangleSymmetricQuadrature.java
@@ -14,9 +14,11 @@ import org.apache.commons.math.analysis.BivariateRealFunction;
  * @author epsilon
  */
 public class TriangleSymmetricQuadrature {
-
-    final static int[] numPts = new int[]{1, 3, 4, 6, 7, 12, 13};
-    final static double[][] weights = new double[][]{
+    
+    public final static int MAX_POWER=5;  // though the arrays weights and barycentricCoordinates have length 7 but the test case can only pass power 5, the reason may caused by the Simpson method or the test function.
+    public final static int MIN_POWER=1;
+    public final static int[] numPts = new int[]{1, 3, 4, 6, 7, 12, 13};
+    public final static double[][] weights = new double[][]{
         {1},
         {1 / 3d, 1 / 3d, 1 / 3d},
         {-27 / 48d, 25 / 48d, 25 / 48d, 25 / 48d},
@@ -24,7 +26,7 @@ public class TriangleSymmetricQuadrature {
         {0.225000000000000, 0.125939180544827, 0.125939180544827, 0.125939180544827, 0.132394152788506, 0.132394152788506, 0.132394152788506},
         {0.050844906370207, 0.050844906370207, 0.050844906370207, 0.116786275726379, 0.116786275726379, 0.116786275726379, 0.082851075618374, 0.082851075618374, 0.082851075618374, 0.082851075618374, 0.082851075618374, 0.082851075618374},
         {-0.149570044467670, 0.175615257433204, 0.175615257433204, 0.175615257433204, 0.053347235608839, 0.053347235608839, 0.053347235608839, 0.077113760890257, 0.077113760890257, 0.077113760890257, 0.077113760890257, 0.077113760890257, 0.077113760890257}};
-    final static double[][] barycentricCoordinates = new double[][]{
+    public final static double[][] barycentricCoordinates = new double[][]{
         {1 / 3d, 1 / 3d, 1 / 3d},
         {2 / 3d, 1 / 6d, 1 / 6d, 1 / 6d, 2 / 3d, 1 / 6d, 1 / 6d, 1 / 6d, 2 / 3d},
         {1 / 3d, 1 / 3d, 1 / 3d, 0.6, 0.2, 0.2, 0.2, 0.6, 0.2, 0.2, 0.2, 0.6},
@@ -33,11 +35,6 @@ public class TriangleSymmetricQuadrature {
         {0.873821971016996, 0.063089014491502, 0.063089014491502, 0.063089014491502, 0.873821971016996, 0.063089014491502, 0.063089014491502, 0.063089014491502, 0.873821971016996, 0.501426509658179, 0.249286745170910, 0.249286745170910, 0.249286745170910, 0.501426509658179, 0.249286745170910, 0.249286745170910, 0.249286745170910, 0.501426509658179, 0.626502499121399, 0.310352451033785, 0.053145049844816, 0.626502499121399, 0.053145049844816, 0.310352451033785, 0.310352451033785, 0.626502499121399, 0.053145049844816, 0.310352451033785, 0.053145049844816, 0.626502499121399, 0.053145049844816, 0.626502499121399, 0.310352451033785, 0.053145049844816, 0.310352451033785, 0.626502499121399},
         {1 / 3d, 1 / 3d, 1 / 3d, 0.479308067841923, 0.260345966079038, 0.260345966079038, 0.260345966079038, 0.479308067841923, 0.260345966079038, 0.260345966079038, 0.260345966079038, 0.479308067841923, 0.869739794195568, 0.065130102902216, 0.065130102902216, 0.065130102902216, 0.869739794195568, 0.065130102902216, 0.065130102902216, 0.065130102902216, 0.869739794195568, 0.638444188569809, 0.312865496004875, 0.048690315425316, 0.638444188569809, 0.048690315425316, 0.312865496004875, 0.312865496004875, 0.638444188569809, 0.048690315425316, 0.312865496004875, 0.048690315425316, 0.638444188569809, 0.048690315425316, 0.638444188569809, 0.312865496004875, 0.048690315425316, 0.312865496004875, 0.638444188569809}
     };
-//    static {
-//        for(int i=0;i<weights.length;i++){
-//            System.out.println(Arrays.toString(weights[i]));
-//        }
-//    }
 
     public static double[] getBarycentricCoordinates(int power, boolean copy) {
         if (copy) {
@@ -48,9 +45,23 @@ public class TriangleSymmetricQuadrature {
     }
 
     public static double[] getBarycentricCoordinates(int power) {
-        return getBarycentricCoordinates(power, false);
+        return getBarycentricCoordinates(power, true);
     }
 
+    public static int getPositions(double x1,double y1,double x2,double y2,double x3,double y3,int power,double [] results){
+        double [] coords=barycentricCoordinates[power-1];
+        int numPt=numPts[power-1];
+        for(int i=0;i<numPts[power-1];i++){
+            results[i*2]=coords[i*3]*x1+coords[i*3+1]*x2+coords[i*3+2]*x3;
+            results[i*2+1]=coords[i*3+1]*y1+coords[i*3+2]*y2+coords[i*3+2]*y3;
+        }
+        return numPt;
+    }
+    
+    public static int getPositions(double x1,double y1,double x2,double y2,double x3,double y3,int power){
+        return getPositions(x1, y1, x2, y2, y3, y3, power, new double[2*numPts[power-1]]);
+    }
+    
     public static double[] getWeights(int power, boolean copy) {
         if (copy) {
             return Arrays.copyOf(weights[power - 1], weights[power - 1].length);
@@ -60,7 +71,7 @@ public class TriangleSymmetricQuadrature {
     }
     
     public static double[] getWeights(int power){
-        return getWeights(power,false);
+        return getWeights(power,true);
     }
 
     public static double quadrate(double x1, double y1, double x2, double y2, double x3, double y3, int power, BivariateRealFunction fun, double area) throws FunctionEvaluationException {
---
+++ test/net/epsilony/math/util/TriangleSymmetricQuadratureTest.java
@@ -0,0 +1,122 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.epsilony.math.util;
+
+import java.util.Arrays;
+import java.util.Random;
+import net.epsilony.math.analysis.SimpsonIntegratorOnTriangleDomain;
+import org.apache.commons.math.analysis.BivariateRealFunction;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static net.epsilony.math.util.TriangleSymmetricQuadrature.*;
+
+/**
+ *
+ * @author epsilon
+ */
+public class TriangleSymmetricQuadratureTest {
+
+    public TriangleSymmetricQuadratureTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Test
+    public void testQuadrate_8args() throws Exception {
+        System.out.println("quadrate");
+        double x1 = 0.0;
+        double y1 = 0.0;
+        double x2 = 10;
+        double y2 = 0.0;
+        double x3 = 5;
+        double y3 = 10;
+        double dis = 5;
+        int repeat = 1;
+        SimpsonIntegratorOnTriangleDomain integrator=new SimpsonIntegratorOnTriangleDomain();
+        integrator.integrator.setRelativeAccuracy(1e-12);  //the best that can pass the test! The bottle neck is the SimpsonIntegrator
+        for (int i = 0; i < repeat; i++) {
+            for (int power = MIN_POWER; power <= MAX_POWER; power++) {   
+                BivariateRealFunction fun = new RandomBivariatePolynomial(power);
+                System.out.println("Power: "+ power +" Repeat"+i);
+                System.out.println("function:"+fun);
+                double x1t=x1+new Random().nextDouble()*dis;
+                double x2t=x2+new Random().nextDouble()*dis;
+                double x3t=x3+new Random().nextDouble()*dis;
+                double y1t=y1+new Random().nextDouble()*dis;
+                double y2t=y2+new Random().nextDouble()*dis;
+                double y3t=y3+new Random().nextDouble()*dis;
+                System.out.println(String.format("(%f,%f)-(%f,%f)-(%f,%f)", x1t,y1t,x2t,y2t,x3t,y3t));
+                System.out.println("面积 = "+EYMath.triangleArea(x1t, y1t, x2t, y2t, x3t, y3t));
+                
+                double simpsonResult = integrator.integrate(x1t, y1t, x2t, y2t, x3t, y3t, fun);
+                double result = TriangleSymmetricQuadrature.quadrate(x1t, y1t, x2t, y2t, x3t, y3t, power, fun);
+                System.out.println("Simpson Result: "+simpsonResult+" result: "+result);
+                assertEquals(simpsonResult, result, Math.abs(simpsonResult*integrator.integrator.getRelativeAccuracy()));
+                System.out.println("");
+            }
+        }
+    }
+    
+    @Test
+    public void testRandomBivariatePolynomial(){
+        System.out.println("Test the sample use function");
+        RandomBivariatePolynomial fun=new RandomBivariatePolynomial(3);
+        System.out.println(fun);
+        int repeat=100;
+        for(int i=0;i<repeat;i++){
+            double x=new Random().nextDouble();
+            double y=new Random().nextDouble();
+            double[] coefs=fun.coefs;
+            double expResult=coefs[0]+coefs[1]*x+coefs[2]*y+coefs[3]*x*x+coefs[4]*x*y+coefs[5]*y*y+coefs[6]*x*x*x+coefs[7]*x*x*y+coefs[8]*x*y*y+coefs[9]*y*y*y;
+            double result=fun.value(x, y);
+            assertEquals(expResult, result,1e-10);
+        }
+    }
+    
+    public static class RandomBivariatePolynomial implements BivariateRealFunction{
+        double[] coefs;
+        int power;
+        @Override
+        public double value(double x,double y){
+            double[] t=new double[power+1];
+            double result=coefs[0];
+            t[0]=1;
+            for(int i=1,index=1;i<=power;i++){
+                for(int j=i;j>0;j--){
+                    t[j]=t[j-1]*y;
+                }
+                t[0]*=x;
+                for(int j=0;j<=i;j++){
+                    result+=t[j]*coefs[index++];
+                }
+            }
+            return result;
+        }
+
+        public RandomBivariatePolynomial(int power) {
+            this.power = power;
+            coefs=new double[((power+1)*(power+2))/2];
+            for(int i=0;i<coefs.length;i++){
+                coefs[i]=new Random().nextDouble();
+            }
+        }
+
+        @Override
+        public String toString() {
+            return Arrays.toString(coefs);
+        }
+                  
+    }
+}
+
+
