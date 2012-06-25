e5ce6f202d7f05134ec44ad233387e626c615cb7
Man YUAN <epsilonyuan@gmail.com>
2012-06-25 12:50:35 +0800

    1. modified EquationSolvers, for further use by Simpmeshfree. Haven't been tested

--- src/net/epsilony/utils/math/EquationSolvers.java
+++ src/net/epsilony/utils/math/EquationSolvers.java
@@ -9,9 +9,18 @@ import java.util.logging.Logger;
 import no.uib.cipr.matrix.DenseVector;
 import no.uib.cipr.matrix.Matrix;
 import no.uib.cipr.matrix.MatrixEntry;
+import no.uib.cipr.matrix.sparse.AMG;
+import no.uib.cipr.matrix.sparse.CGS;
+import no.uib.cipr.matrix.sparse.CompRowMatrix;
+import no.uib.cipr.matrix.sparse.DefaultIterationMonitor;
 import no.uib.cipr.matrix.sparse.FlexCompRowMatrix;
+import no.uib.cipr.matrix.sparse.GMRES;
+import no.uib.cipr.matrix.sparse.ILU;
+import no.uib.cipr.matrix.sparse.ILUT;
+import no.uib.cipr.matrix.sparse.IterationMonitor;
 import no.uib.cipr.matrix.sparse.IterativeSolver;
 import no.uib.cipr.matrix.sparse.IterativeSolverNotConvergedException;
+import no.uib.cipr.matrix.sparse.Preconditioner;
 
 /**
  *
@@ -62,21 +71,33 @@ public class EquationSolvers {
         private boolean upperOnly;
         IterativeSolver solver;
 
+        public SparseIterative(boolean upperOnly) {
+            this.upperOnly=upperOnly;
+        }
+
         @Override
         public DenseVector solve(Matrix matrix, DenseVector vector) {
-            FlexCompRowMatrix mat = new FlexCompRowMatrix(matrix.numRows(), matrix.numRows());
+            FlexCompRowMatrix actMat = new FlexCompRowMatrix(matrix.numRows(), matrix.numColumns());
             for (MatrixEntry me : matrix) {
                 int row = me.row();
                 int column = me.column();
                 double value = me.get();
-                mat.set(row, column, value);
-                if (upperOnly && row != column) {
-                    mat.set(column, row, value);
+                actMat.set(row, column, value);
+                if (upperOnly && row < column) {
+                    actMat.set(column, row, value);
                 }
             }
             DenseVector result = new DenseVector(vector.size());
+            IterationMonitor iterMonitor=new DefaultIterationMonitor();
+            if(null==solver){
+                solver=new CGS(new DenseVector(result.size()));
+                Preconditioner pc=new AMG();
+                pc.setMatrix(actMat);
+                solver.setPreconditioner(pc);
+                solver.setIterationMonitor(iterMonitor);               
+            }
             try {
-                solver.solve(matrix, vector, result);
+                solver.solve(actMat, vector, result);
             } catch (IterativeSolverNotConvergedException ex) {
                 Logger.getLogger(EquationSolvers.class.getName()).log(Level.SEVERE, null, ex);
                 throw new IllegalStateException();
@@ -87,6 +108,11 @@ public class EquationSolvers {
         public SparseIterative(IterativeSolver solver, boolean upperOnly) {
             this.upperOnly = upperOnly;
             this.solver = solver;
+        }
+
+        @Override
+        public String toString() {
+            return "SparseIterative{" + "solver=" + solver + '}';
         }
     }
 }
