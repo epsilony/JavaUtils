6f3516b353eebe8db790a9cd3ac67fa06e4f3cec
Man YUAN <epsilonyuan@gmail.com>
2012-11-04 22:14:07 +0800

    Move general generalduty classes in.

---
+++ src/net/epsilony/utils/Unitable.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.epsilony.utils;
+
+/**
+ *
+ * @author epsilon
+ */
+public interface Unitable<A>{
+
+    /**
+     * 合并所有曾经产生的分身到本实例
+     */
+    void uniteIn(A a);
+}
---
+++ src/net/epsilony/utils/WithId.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.epsilony.utils;
+
+/**
+ *
+ * @author epsilon
+ */
+public interface WithId {
+
+    int getId();
+
+    void setId(int id);
+    
+}
